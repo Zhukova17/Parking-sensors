d552bb5c74087bf6f3e375cdb41c98f23b8c2291
Zhukova17 <145455351+Zhukova17@users.noreply.github.com>
2024-12-17 11:45:11 +0300

    Add files via upload

---
+++ Parking sensors/Entrance.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Parking_sensors
+{
+    /// <summary>
+    /// Логика взаимодействия для Entrance.xaml
+    /// </summary>
+    public partial class Entrance : Window
+    {
+        public Entrance()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
